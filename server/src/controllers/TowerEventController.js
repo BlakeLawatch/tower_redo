c2449e62df2c38983bbb0f051cab4cdcec82587d
Blake Lawatch <blakelawatch@gmail.com>
2023-11-07 07:55:00 -0700

    First set of postman tests completed

--- server/src/controllers/TowerEventController.js
+++ server/src/controllers/TowerEventController.js
@@ -7,10 +7,33 @@ export class TowerEventController extends BaseController {
     constructor() {
         super('api/events')
         this.router
-
+            .get('', this.getEvents)
+            .get('/:eventId', this.getEventById)
             .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.createEvent)
+            .put('/:eventId', this.editEvent)
+            .delete('/:eventId', this.cancelEvent)
     }
+    async getEvents(req, res, next) {
+        try {
+            const eventId = req.params.eventId
+            const towerEvents = await towerEventsService.getEvents(eventId)
+            return res.send(towerEvents)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getEventById(req, res, next) {
+        try {
+            const eventId = req.params.eventId
+            const getEvent = await towerEventsService.getEventById(eventId)
+            return res.send(getEvent)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
     async createEvent(req, res, next) {
         try {
             const eventData = req.body
@@ -18,6 +41,27 @@ export class TowerEventController extends BaseController {
             eventData.creatorId = userId
             const newEvent = await towerEventsService.createEvent(eventData)
             return res.send(newEvent)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async editEvent(req, res, next) {
+        try {
+            const userInfo = req.body
+            const userId = req.userInfo.id
+            const eventId = req.params.eventId
+            const editedEvent = await towerEventsService.editEvent(userInfo, userId, eventId)
+            return res.send(editedEvent)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async cancelEvent(req, res, next) {
+        try {
+            const userId = req.userInfo.id
+            const eventId = req.params.eventId
+            const deletedEvent = await towerEventsService.cancelEvent(userId, eventId)
+            return res.send(deletedEvent)
         } catch (error) {
             next(error)
         }
--- server/src/services/TowerEventsService.js
+++ server/src/services/TowerEventsService.js
@@ -1,10 +1,46 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest, Forbidden } from "../utils/Errors.js"
 
 class TowerEventsService {
+
+    async getEvents(eventId) {
+        const towerEvents = await dbContext.TowerEvent.find(eventId).populate('ticketCount')
+        return towerEvents
+    }
+
+    async getEventById(eventId) {
+        const getEvent = await dbContext.TowerEvent.findById(eventId)
+        return getEvent
+    }
 
     async createEvent(eventData) {
         const newEvent = (await dbContext.TowerEvent.create(eventData)).populate('creator ticketCount')
         return newEvent
+    }
+
+    async editEvent(userInfo, userId, eventId) {
+        const editedEvent = await dbContext.TowerEvent.findById(eventId)
+        if (editedEvent.creatorId != userId) {
+            throw new BadRequest('Not yours to update')
+        }
+        if (editedEvent.isCanceled) {
+            throw new BadRequest('this event has been cancelled')
+        }
+        editedEvent.name = userInfo.name
+        editedEvent.description = userInfo.description
+        await editedEvent.save()
+        return editedEvent
+    }
+
+    async cancelEvent(userId, eventId) {
+        const cancelledEvent = await this.getEventById(eventId)
+        if (cancelledEvent.creatorId != userId) {
+            throw new Forbidden('Not yours to delete')
+        }
+
+        cancelledEvent.isCanceled = !cancelledEvent.isCanceled
+        await cancelledEvent.save()
+        return cancelledEvent
     }
 }
 
