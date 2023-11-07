89d2193458061fc8d0c18bac470b0688e4fede3e
Blake Lawatch <blakelawatch@gmail.com>
2023-11-07 07:15:35 -0700

    Login component/create in place

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -18,6 +18,9 @@
                 Manage Account
               </div>
             </router-link>
+            <div class="list-group-item dropdown-item list-group-item-action">
+              Create Event
+            </div>
             <div class="list-group-item dropdown-item list-group-item-action text-danger selectable" @click="logout">
               <i class="mdi mdi-logout"></i>
               logout
